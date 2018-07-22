f950d62c2b666627f19da43de6f68744112d7cb5
Richard Chen <richard.c.chen@gmail.com>
2018-07-22 22:26:58 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -9,3 +9,15 @@ end
 def first_element(array)
   array[0]
 end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  
+end
